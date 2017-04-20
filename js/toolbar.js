465ec335b9863db9b5cc750601484c5cd5ff0758
Ftujio <svetlin954@abv.bg>
2017-04-20 19:58:46 +0300

    Fix in the logic.

--- js/toolbar.js
+++ js/toolbar.js
@@ -6,44 +6,42 @@ for(let i = 0; i < toolNum; i++){
 	elem[i].addEventListener("click", function(e){ setCurrentTool(e, i) });
 }
 
-canvas.addEventListener("click", function(e){
-	console.log('using ', currentTool);
+console.log('using ', currentTool);
 
-	switch(currentTool){
-		case 'line':
-			line(e);
-			break;
-		case 'circle':
-			circle(e);
-			break;
-		case 'square':
-			square(e);
-			break;
-		case 'eraser':
-			eraser(e);
-			break;
-		case 'undo':
-			undo(e);
-			break;
-		case 'bin':
-			bin(e);
-			break;
-		case 'save':
-			save(e);
-			break;
-		default:
-			brush(e);
-			console.log('Brush');
-	}
-});
+switch(currentTool){
+	case 'line':
+		line();
+		break;
+	case 'circle':
+		circle();
+		break;
+	case 'square':
+		square();
+		break;
+	case 'eraser':
+		eraser();
+		break;
+	case 'undo':
+		undo();
+		break;
+	case 'bin':
+		bin();
+		break;
+	case 'save':
+		save();
+		break;
+	default:
+		brush();
+		console.log('Brush');
+}
 
 function brush(e){
-	let x = e.offsetX;
-	let y = e.offsetY;
-	c.beginPath();
-	c.arc(x, y, 50, 0, 2*Math.PI);
-	c.fill();
-	c.stroke();
+	canvas.addEventListener("click", function(e){
+		c.beginPath();
+		c.arc(e.offsetX, e.offsetY, 10, 0, 2*Math.PI);
+		c.fill();
+		c.stroke();
+	});
 }
 
 function setCurrentTool(e, i){
