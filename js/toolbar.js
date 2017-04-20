f05df10f48eff0dec010901d91e16c74cc38b28f
Ftujio <svetlin954@abv.bg>
2017-04-20 19:29:59 +0300

    Handling the current tool.

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,10 +1,20 @@
 let toolNum = document.getElementById("tools-toolbar").children[0].childElementCount;
 let elem = document.getElementById("tools-toolbar").children[0].children;
+var currentTool = 'brush';
 
 for(let i = 0; i < toolNum; i++){
 	elem[i].addEventListener("click", function(e){ setCurrentTool(e, i) });
 }
 
+canvas.addEventListener("click", function(){
+	console.log('using ', currentTool);
+
+	if(currentTool == 'brush'){
+		
+	}
+});
+
 function setCurrentTool(e, i){
-	
+	currentTool = elem[i].id;
+	console.log(elem[i].id ,' tool selected');
 }
