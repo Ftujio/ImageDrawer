cdeb80bb8caad6b3ac63fbea2969b0985c9a6375
Ftujio <svetlin954@abv.bg>
2017-04-27 22:48:14 +0300

    Code refactor.

--- js/setup.js
+++ js/setup.js
@@ -3,8 +3,7 @@ var c = canvas.getContext("2d");
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-var save = document.getElementById("save");
-save.addEventListener("click", function(e){
+var save = document.getElementById("save").addEventListener("click", function(e){
 	console.log('Image saved');
 	console.log(c);
 	this.href = canvas.toDataURL('image/png');
