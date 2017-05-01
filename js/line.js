d2e5588449a812878d3cae75121d65005740fff3
Ftujio <svetlin954@abv.bg>
2017-05-01 18:29:24 +0300

    Refactored the line tool code.

--- js/line.js
+++ js/line.js
@@ -1,20 +1,23 @@
 c.lineCap = "round";
 c.lineWidth = 10;
-console.log(canvas);
 
 function line(){
 	removeEventListeners();
 
-	canvas.addEventListener("mousedown", function(e){
-		c.beginPath();
-		c.moveTo(e.offsetX,e.offsetY) 
-		console.log("offsetX: ", e.offsetX, " offsetY: ", e.offsetY);
-	});
-	canvas.addEventListener("mouseup", function(e){ 
-		c.lineTo(e.offsetX, e.offsetY);
-		c.strokeStyle = color;
-		c.stroke();
-		c.closePath();
-		console.log("offsetX: ", e.offsetX, " offsetY: ", e.offsetY);
-	});
+	canvas.addEventListener("mousedown", first);
+	canvas.addEventListener("mouseup", second);
+}
+
+var first = function(e){
+	c.beginPath();
+	c.moveTo(e.offsetX,e.offsetY) 
+	console.log("offsetX: ", e.offsetX, " offsetY: ", e.offsetY);
+}
+
+var second = function(e){
+	c.lineTo(e.offsetX, e.offsetY);
+	c.strokeStyle = color;
+	c.stroke();
+	c.closePath();
+	console.log("offsetX: ", e.offsetX, " offsetY: ", e.offsetY);
 }
