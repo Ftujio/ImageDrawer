1786854888df0ff5f9d79d8f98a95c14feb83b90
Ftujio <svetlin954@abv.bg>
2017-05-04 19:43:16 +0300

    Fixed bug with eraser and stroke disappearing.
    * Fixed bug #4

--- js/eraser.js
+++ js/eraser.js
@@ -2,6 +2,7 @@ let dragging2 = false;
 
 function eraser(){
 	removeEventListeners();
+	c.lineWidth = 20;
 
 	canvas.addEventListener("mousedown", engage2);
 	canvas.addEventListener("mousemove", down2);
@@ -25,7 +26,6 @@ function drawPath2(x, y, radius){
 	if(dragging2){
 		c.lineTo(x, y);
 		c.strokeStyle = "white";
-		c.lineWidth = 20;
 		c.stroke();
 		c.beginPath();
 		c.arc(x, y, radius, 0, 2*Math.PI);
