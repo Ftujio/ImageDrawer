1121e859a875a559ac1f6119238096bddcdbd15b
Ftujio <svetlin954@abv.bg>
2017-06-30 22:59:07 +0300

    JQuery is tested and is working.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
 	console.log('jquery is working');
+	let a = $(".test").text();
+	let elem = $("<p></p>").text(a);
+	$('#wrapper').append(elem);
+	console.log(a);
 });
