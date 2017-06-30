92c022fcbf42c78802ee6530bbc4cdf60fdc629d
Ftujio <svetlin954@abv.bg>
2017-06-30 23:10:54 +0300

    Paperjs is working.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-	
-});
+console.log('app loaded');
+window.onload = function(){
+	var canvas = document.getElementById('drawing');
+	paper.setup(canvas);
+	var path = new paper.Path();
+	path.strokeColor = 'black';
+	var start = new paper.Point(100, 100);
+	path.moveTo(start);
+	path.lineTo(start.add([ 200, -50 ]));
+	paper.view.draw();
+}
