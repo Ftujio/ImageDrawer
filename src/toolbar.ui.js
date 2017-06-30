e40599bde4f8b5502509f3b739764e6c0a67dd74
Ftujio <svetlin954@abv.bg>
2017-07-01 00:35:32 +0300

    Definded the tools.

--- src/toolbar.ui.js
+++ src/toolbar.ui.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-	
+	let toolbar = [
+		{
+			"name": "brush",
+			"icon-src": ""
+		},
+		{
+			"name": "line",
+			"icon-src": ""
+		},
+		{
+			"name": "circle",
+			"icon-src": ""
+		},
+		{
+			"name": "rectangle",
+			"icon-src": ""
+		},
+		{
+			"name": "eraser",
+			"icon-src": ""
+		},
+		{
+			"name": "delete",
+			"icon-src": ""
+		},
+		{
+			"name": "save",
+			"icon-src": ""
+		}
+	];
 });
