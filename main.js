aaae7d080a6ad0d9a138b9f86e7ca9fd57e86656
Ftujio <svetlin954@abv.bg>
2017-07-01 00:23:00 +0300

    Changed the window resolution to be "hd".

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let win;
 function createWindow(){
 	// Create browser window
 	win = new BrowserWindow({
-		width: 1200,
-		height: 800
+		width: 1280,
+		height: 720
 	});
 
 	win.loadURL(url.format({
