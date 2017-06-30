19decf632dfe7211b24c1b17919414b376090c1d
Ftujio <svetlin954@abv.bg>
2017-06-30 23:06:52 +0300

    Removed test code.

--- renderer.js
+++ renderer.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-	console.log('jquery is working');
-	let a = $(".test").text();
-	let elem = $("<p></p>").text(a);
-	$('#wrapper').append(elem);
-	console.log(a);
+	
 });
